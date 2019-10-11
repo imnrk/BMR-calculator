f806d6380027be7cf854f814c9befdd586080d03
Nishant Rajiv Kore <nkore@andrew.cmu.edu>
2019-10-11 15:09:42 -0400

    Added features upto coach marks

--- execution.js
+++ execution.js
@@ -38,28 +38,97 @@ function calc() {
   bmr = Math.round(val);
 
   document.getElementById('bmr').innerHTML = ans;
+  buttoninput();
 }
-//Hidden information
+
+//Hidden information - Focus pattern
 function calneed() {
   document.getElementById('dropdown').style.display = 'block';
   document.getElementById('exbtn').style.display = 'block';
+  document.getElementById('settingsbtn').style.display = 'block';
+  buttoninput();
 }
-//Expandable Input
+
+//Expandable Input - Focus pattern
 function calories() {
   //  console.log(parseFloat(document.getElementById("dropdown").value));
   let ex = parseFloat(document.getElementById('dropdown').value);
   document.getElementById('calorie').innerHTML =
     'Daily Caloric Need is: ' + ex * bmr + ' calories.';
 }
-//Popover
+
+//Popover - Help pattern
 function exercise_help() {
   alert(
     'Choose an option from the dropdown that best signifies your daily exercise regimen'
   );
 }
 
-//inputhint
+//inputhints - Help pattern
 function inputhint() {
   document.getElementById('hint').innerHTML =
     'Please choose an option from dropdown that describes your physical activity';
+}
+
+//Overflow Menu -Focus Pattern
+function overflowmenu() {
+  //https://stackoverflow.com/questions/362614/calling-onclick-on-a-radiobutton-list-using-javascript
+  let x = document.getElementById('calorie').innerHTML;
+  x = parseFloat(x.split(' ')[4]);
+  var joules = x * 4.184;
+  var kcal = x * 0.001;
+  var sj = document.getElementById('setjoule').checked;
+  var sk = document.getElementById('setkcal').checked;
+  var text = 'Daily need in ';
+  if (sj) {
+    text += 'joules is ' + joules + ' joules';
+  } else if (sk) {
+    text += 'kcal is ' + kcal + ' Kcal';
+  }
+
+  document.getElementById('convertedcalorie').innerHTML = text;
+}
+
+function getoverflowmenu() {
+  document.getElementById('convert').style.display = 'block';
+  document.getElementById('convertbtn').style.display = 'block';
+}
+
+//Wizard - Focus pattern
+function wizardcalc() {
+  document.getElementById('age').value = 25;
+  document.getElementById('height').value = 6;
+  document.getElementById('weight').value = 120;
+  let val =
+    document.getElementById('weight').value * 10 +
+    (document.getElementById('height').value +
+      document.getElementById('inchheight').value * 0.0833) *
+      6.25 -
+    document.getElementById('age').value * 5 +
+    5;
+  let ans = 'BMR : ' + Math.round(val);
+  bmr = Math.round(val);
+
+  document.getElementById('bmr').innerHTML = ans;
+  buttoninput();
+}
+
+//Coachmarks - Focus pattern
+function ageinput() {
+  document.getElementById('stepage').style.display = 'none';
+  document.getElementById('stepheight').style.display = 'block';
+}
+
+function heightinput() {
+  document.getElementById('stepheight').style.display = 'none';
+  document.getElementById('stepweight').style.display = 'block';
+}
+
+function weightinput() {
+  document.getElementById('stepweight').style.display = 'none';
+  document.getElementById('stepbutton').style.display = 'block';
+}
+
+function buttoninput() {
+  document.getElementById('stepbutton').style.display = 'none';
 }
